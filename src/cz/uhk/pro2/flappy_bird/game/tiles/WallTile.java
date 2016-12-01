e97ca068b3a390edcf66e863b38a03d45e4bc5b9
volejon1 <ondrej.volejnik@mensa.cz>
2016-12-01 12:18:02 +0100

    ScvWorldLoader and WallTile updated

--- src/cz/uhk/pro2/flappy_bird/game/tiles/WallTile.java
+++ src/cz/uhk/pro2/flappy_bird/game/tiles/WallTile.java
@@ -2,15 +2,23 @@ package cz.uhk.pro2.flappy_bird.game.tiles;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
 
 import cz.uhk.pro2.flappy_bird.game.Tile;
 
 public class WallTile implements Tile {
-
+	Image image;
+	
+	public WallTile (Image image){
+		this.image = image;
+	}
+	
 	@Override
 	public void draw(Graphics g, int x, int y) {
-		g.setColor(Color.MAGENTA);
-		g.fillRect(x, y, Tile.SIZE, Tile.SIZE);
+		/*g.setColor(Color.MAGENTA);
+		g.fillRect(x, y, Tile.SIZE, Tile.SIZE);*/
+		g.drawImage(image, x, y, new ImageObserver());
 	}
 
 }
--- src/cz/uhk/pro2/flappy_bird/service/CsvWorldLoader.java
+++ src/cz/uhk/pro2/flappy_bird/service/CsvWorldLoader.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 import cz.uhk.pro2.flappy_bird.game.Tile;
 import cz.uhk.pro2.flappy_bird.game.World;
@@ -24,21 +26,32 @@ public class CsvWorldLoader implements WorldLoader {
 			String[] line = br.readLine().split(";");
 			int typeCount = Integer.parseInt(line[0]);
 			//skips type definition cells
+			Map<String, Tile> tileTypes = new HashMap<>();
 			for(int i = 0; i < typeCount; i++){
-				br.readLine();
+				line = br.readLine().split(";");
+				String tileShort = line[0];
+				String tileType = line[1];
+				int spriteStartX = Integer.parseInt(line[2]);
+				int spriteStartY = Integer.parseInt(line[3]);
+				int spriteSizeX = Integer.parseInt(line[4]);
+				int spriteSizeY = Integer.parseInt(line[5]);
+				String spriteURL = line[6];
+				Tile tile = createTile(tileType, spriteStartX, spriteStartY, spriteSizeX, spriteSizeY);
+				tileTypes.put(key, value);
 			}
 			line = br.readLine().split(";");
 			int rows = Integer.parseInt(line[0]);
 			int columns = Integer.parseInt(line[1]);
 			Tile[][] tiles = new Tile[rows][columns];
 			System.out.println(rows + "," + columns);
+			Tile tile = new WallTile();
 			for(int i = 0; i < rows; i++){
 				line = br.readLine().split(";");
 				for(int j = 0; j < columns; j++){
 				//if the cell exists read it, else input empty cell
 					cell = j < line.length ? line[j] : "";
 					if(!"".equals(cell)){
-						tiles[i][j] = new WallTile();
+						tiles[i][j] = tileTypes.get(cell);
 					}
 				}
 			}
@@ -48,6 +61,10 @@ public class CsvWorldLoader implements WorldLoader {
 		} catch (IOException e) {
 			throw new RuntimeException("Error reading the file", e);
 		}
+	}
+
+	private Tile createTile(String tileType, int spriteStartX, int spriteStartY, int spriteSizeX, int spriteSizeY) {
+		return null;
 	}
 
 }
